0dd0d9777e11367b637f05150e58263808444a94
htraing <hei-hong.traing@younited-credit.fr>
2020-08-03 17:38:51 +0200

    init router

---
+++ libs/navigation/router/src/lib/store/router.reducer.ts
@@ -0,0 +1,10 @@
+import * as fromRouter from '@ngrx/router-store';
+import { ActionReducerMap } from '@ngrx/store';
+import { routerReducer } from '@ngrx/router-store';
+
+export interface StoreRootState {
+    router: fromRouter.RouterReducerState<any>;
+  }
+export const reducers: ActionReducerMap<StoreRootState> = {
+    router: routerReducer,
+};
