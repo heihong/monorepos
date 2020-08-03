c77bee9d1d270eab8d03051e6a94bf8c5407b3a9
htraing <hei-hong.traing@younited-credit.fr>
2020-08-03 11:19:38 +0200

    init navigation

--- apps/navigation/src/app/app.module.ts
+++ apps/navigation/src/app/app.module.ts
@@ -1,11 +1,42 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { RouterModule } from '@angular/router';
 
 import { AppComponent } from './app.component';
+import { PATH } from './settings/path.settings' 
+
+const routes = [
+  {
+    path: PATH.Step_A,
+    loadChildren: () =>
+      import(
+        '@monorepos/navigation/step-a'
+      ).then(m => m.StepAModule)
+  },{
+    path: PATH.Step_B,
+    loadChildren: () =>
+      import(
+        '@monorepos/navigation/step-b'
+      ).then(m => m.StepBModule)
+  },{
+    path: PATH.Step_C,
+    loadChildren: () =>
+      import(
+        '@monorepos/navigation/step-c'
+      ).then(m => m.StepCModule)
+  },{
+    path: PATH.Step_D,
+    loadChildren: () =>
+      import(
+        '@monorepos/navigation/step-d'
+      ).then(m => m.StepDModule)
+  },
+]
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule],
+  imports: [BrowserModule,
+    RouterModule.forRoot(routes)],
   providers: [],
   bootstrap: [AppComponent],
 })
---
+++ apps/navigation/src/app/settings/path.settings.ts
@@ -0,0 +1,7 @@
+export const enum PATH {
+    Step_A = 'step-a',
+    Step_B = 'step-b',
+    Step_C = 'step-c',
+    Step_D = 'step-d'
+
+}
